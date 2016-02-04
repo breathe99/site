0b156934faf5bc037434a328a56d4c49f22a4095
themezzilla <crollins199@gmail.com>
2016-02-03 21:42:19 -0600

    Animations!

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,13 @@
     $('.click').click(function() {
       if (!$(this).hasClass('scale')) {
         $(this).addClass('scale');
-        $(this).siblings('.col').hide();
+        $(this).siblings('.col').addClass('shrink');
+        $(this).siblings('.col').find('p').hide();
       }
       else {
         $(this).removeClass('scale');
-        $(this).siblings('.col').show();
+        $(this).siblings('.col').removeClass('shrink');
+        $(this).siblings('.col').find('p').show();
       }
     });
 
