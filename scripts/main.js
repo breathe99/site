739a101f7bf453588cdadd651c2016076705170d
themezzilla <crollins199@gmail.com>
2016-02-08 20:15:06 -0600

    Added stock hidden content sections and JS to show/hide on cards, AJAX call for map data

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,31 @@
         $(this).siblings('.col').removeClass('shrink');
         $(this).siblings('.col').find('p').show();
       }
+      // Show hidden content
+      if($(this).find('.text-reveal').hasClass('hidden')) {
+        $(this).find('.text-reveal').removeClass('hidden');
+      }
+      else {
+        $(this).find('.text-reveal').addClass('hidden');
+      }
     });
 
+    // Get the latest map data on page load
+    $.ajax({
+      url: 'http://dexpi.ddns.net/aqi',
+      dataType: 'JSON',
+      type: 'GET',
+      async: false,
+      success: function (data) {
+        console.log(JSON.stringify(data));
+        console.log("something");
+      },
+      error: function (response) {
+        var r = jQuery.parseJSON(response.responseText);
+        console.log("Message: " + r.Message);
+        console.log("StackTrace: " + r.StackTrace);
+        console.log("ExceptionType: " + r.ExceptionType);
+      }
+    });
   });
 }( jQuery ));
