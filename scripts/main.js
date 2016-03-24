32531da3ab7b69e6456c53134185c75ef28f3246
Jake Dexheimer <jakedex@gmail.com>
2016-03-23 22:42:21 -0500

    Add new logo, aqi value positioning

--- scripts/main.js
+++ scripts/main.js
@@ -21,9 +21,8 @@
 
     // ------------ GSAP expand ------------
     var cards = document.getElementsByClassName('card-div'),
-        map_date = $('.map-date');
-        //homepage cards to expand on hover
-      partner_card = [cards[1], cards[0], cards[3], cards[2]],
+    //homepage cards to expand on hover
+    partner_card = [cards[1], cards[0], cards[3], cards[2]],
       numCards = cards.length,
       timelines = [],
       card_inners = document.getElementsByClassName('card-inner'),
@@ -75,18 +74,6 @@
         expand(e, i);
       }
     }
-    
-    // update out card's date/time
-    function updateDateTime() {
-      var now = new Date();
-      var formatted = now.getHours() + ":" + now.getMinutes() + " | " + (now.getMonth() + 1) + "/" + now.getDate() + "/" + now.getFullYear();
-      
-      map_date[0].innerHTML = formatted;
-      map_date[1].innerHTML = formatted;
-    }
-    
-    // update on load TODO
-    updateDateTime();
 
     //play card timeline on hover
     function expand(e, i) {
@@ -94,7 +81,7 @@
       if (i === 0) {
         cityPointTimelines[selectedPointInd].play();
       }
-  
+
       cards[i].classList.remove('hover');
       card_inners[i].classList.add('hidden');
       card_expands[i].classList.remove('hidden');
@@ -108,7 +95,7 @@
       if (i === 0) {
         cityPointTimelines[selectedPointInd].pause();
       }
-      
+
       cards[i].classList.add('hover');
       card_inners[i].classList.remove('hidden');
       card_expands[i].classList.add('hidden');
@@ -139,7 +126,7 @@
       selectedLi.classList.remove('map-city');
       selectedLi = getCityLI(i);
       selectedLi.classList.add('map-city');
-      
+
       // switch card cover title city
       outerCityTitle.innerHTML = cities[i];
 
@@ -150,29 +137,33 @@
       selectedPointInd = i;
       points[selectedPointInd].classList.add('pulsing');
       cityPointTimelines[selectedPointInd].play();
-      
+
       // switch aqi data
       aqiValue[0].innerHTML = aqiData[i];
       aqiValue[1].innerHTML = aqiData[i];
     }
-    
+
     //function for each city point's 'pulse' animation
     function createPulsingTimeline(i) {
       var timeline = new TimelineMax({
         paused: true,
         repeat: -1
       });
-      
+
       timeline.from(points[i], 1, {
-        css: {r: 2.5},
+        css: {
+          r: 2.5
+        },
         ease: Expo.easeOut
       }, 0);
-      
+
       timeline.to(points[i], 1.5, {
-        css: {r: '+=8'},
+        css: {
+          r: '+=8'
+        },
         ease: Expo.easeOut
       }, 0);
-      
+
       cityPointTimelines[i] = timeline;
     }
 
@@ -212,13 +203,29 @@
 
       }(i));
     }
-    
+
     // create pulsing timelines, assign all listeners
     for (var j = 0; j < numPoints; j += 1) {
       createPulsingTimeline(j);
       assignPointCityListeners(j);
     }
+
+    // -------- update last update time on page load --------
+    // update out card's date/time
+    var map_date = $('.map-date');
     
+    function updateDateTime() {
+      var now = new Date();
+      var formatted = now.getHours() + ":" + now.getMinutes() + " | " + (now.getMonth() + 1) + "/" + now.getDate() + "/" + now.getFullYear();
+
+      map_date[0].innerHTML = formatted;
+      map_date[1].innerHTML = formatted;
+    }
+
+    // update on load TODO
+    updateDateTime();
+
+
     // Get the latest map data on page load
     $.ajax({
       url: 'http://dexpi.ddns.net/aqi',
